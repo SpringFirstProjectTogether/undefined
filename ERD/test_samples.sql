81fa001dfab407c4ed9a8e8e8641f9546edda2b2
Junwoo Kang <junwookang@Junwoos-MacBook-Pro.local>
2023-11-28 00:35:29 +0900

    Feat.junwoo DDL 찐 막 수정

---
+++ ERD/test_samples.sql
@@ -0,0 +1,122 @@
+-- feed_comments 테이블 데이터 조회
+SELECT * FROM feed_comments;
+
+-- feed_likes 테이블 데이터 조회
+SELECT * FROM feed_likes;
+
+-- feed_photos 테이블 데이터 조회
+SELECT * FROM feed_photos;
+
+-- feed_tags 테이블 데이터 조회
+SELECT * FROM feed_tags;
+
+-- feeds 테이블 데이터 조회
+SELECT * FROM feeds;
+
+-- info_careerdetails 테이블 데이터 조회
+SELECT * FROM info_careerdetails;
+
+-- jobs_files 테이블 데이터 조회
+SELECT * FROM jobs_files;
+
+-- info_files 테이블 데이터 조회
+SELECT * FROM info_files;
+
+-- info_url 테이블 데이터 조회
+SELECT * FROM info_url;
+
+-- info_resume 테이블 데이터 조회
+SELECT * FROM info_resume;
+
+-- jobs_positions 테이블 데이터 조회
+SELECT * FROM jobs_positions;
+
+-- info_jobs 테이블 데이터 조회
+SELECT * FROM info_jobs;
+
+-- pg_comments 테이블 데이터 조회
+SELECT * FROM pg_comments;
+
+-- pg_likes 테이블 데이터 조회
+SELECT * FROM pg_likes;
+
+-- pg_positions 테이블 데이터 조회
+SELECT * FROM pg_positions;
+
+-- pg_skills 테이블 데이터 조회
+SELECT * FROM pg_skills;
+
+-- pg_types_url 테이블 데이터 조회
+SELECT * FROM pg_types_url;
+
+-- pr_positions 테이블 데이터 조회
+SELECT * FROM pr_positions;
+
+-- positions 테이블 데이터 조회
+SELECT * FROM positions;
+
+-- posts_files 테이블 데이터 조회
+SELECT * FROM posts_files;
+
+-- project_gallery 테이블 데이터 조회
+SELECT * FROM project_gallery;
+
+-- pr_comments 테이블 데이터 조회
+SELECT * FROM pr_comments;
+
+-- pr_likes 테이블 데이터 조회
+SELECT * FROM pr_likes;
+
+-- pr_skills 테이블 데이터 조회
+SELECT * FROM pr_skills;
+
+-- pr_types_urls 테이블 데이터 조회
+SELECT * FROM pr_types_urls;
+
+-- project_recruits 테이블 데이터 조회
+SELECT * FROM project_recruits;
+
+-- qna_comment_likes 테이블 데이터 조회
+SELECT * FROM qna_comment_likes;
+
+-- qna_comments 테이블 데이터 조회
+SELECT * FROM qna_comments;
+
+-- qna_posts 테이블 데이터 조회
+SELECT * FROM qna_posts;
+
+-- study_skills 테이블 데이터 조회
+SELECT * FROM study_skills;
+
+-- skills 테이블 데이터 조회
+SELECT * FROM skills;
+
+-- study_comments 테이블 데이터 조회
+SELECT * FROM study_comments;
+
+-- study_files 테이블 데이터 조회
+SELECT * FROM study_files;
+
+-- study_likes 테이블 데이터 조회
+SELECT * FROM study_likes;
+
+-- study_posts 테이블 데이터 조회
+SELECT * FROM study_posts;
+
+-- user_port_url 테이블 데이터 조회
+SELECT * FROM user_port_url;
+
+-- urltype 테이블 데이터 조회
+SELECT * FROM urltype;
+
+-- users 테이블 데이터 조회
+SELECT * FROM users;
+
+-- pg_files 테이블 데이터 조회
+SELECT * FROM pg_files;
+
+-- pr_files 테이블 데이터 조회
+SELECT * FROM pr_files;
+
+-- project_files 테이블 데이터 조회
+SELECT * FROM project_files;
